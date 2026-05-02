5e819a97fe7d4e1450e081d0fe4e1beff47859c1
Zes Minkey Young <2149291470@qq.com>
2026-05-02 15:59:05 +0800

    fix(polyfill): 我不信了

--- polyfill.ts
+++ polyfill.ts
@@ -1,13 +1,8 @@
 if (window)
 // @ts-expect-error
-window.OffscreenCanvas = window.OffscreenCanvas ?? new Proxy({}, {
-    construct(target, argArray, newTarget) {
-        const canvas = document.createElement("canvas");
-        canvas.width = argArray[0];
-        canvas.height = argArray[1];
-        return canvas;
-    },
-    get(target, prop) {
-        return HTMLCanvasElement[prop];
-    }
-})
+window.OffscreenCanvas = window.OffscreenCanvas ?? function (...argArray) {
+    const canvas = document.createElement("canvas");
+    canvas.width = argArray[0];
+    canvas.height = argArray[1];
+    return canvas;
+}
